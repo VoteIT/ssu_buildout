9ba107987e17f2f352d795526169e6686d2d4e67
Robin Harms Oredsson <robin@betahaus.net>
2019-08-07 14:32:49 +0200

    Adjust all proposals no one has spoken for to unhandled before starting the decision process

--- src/ssu_theme/ssu_theme/bifall_yrk.py
+++ src/ssu_theme/ssu_theme/bifall_yrk.py
@@ -1,13 +1,18 @@
+# -*- coding: utf-8 -*-
 """ A button to express that someone has spoken for this proposal. ("Yrkat bifall")
 """
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from arche.views.base import BaseView
 from betahaus.viewcomponent import view_action
+from pyramid.httpexceptions import HTTPFound
 from pyramid.renderers import render
 from pyramid.response import Response
 from pyramid.view import view_config
+from voteit.core.models.interfaces import IAgendaItem
 from voteit.core.models.interfaces import IProposal
+from voteit.core.security import MODERATE_MEETING
+from voteit.core.security import unrestricted_wf_transition_to
 
 
 ATTR_NAME = '_bifall_yrk_enabled'
@@ -31,6 +36,36 @@ def bifall_yrk_btn(context, request):
     enable = bool(request.GET.get('e', None))
     setattr(context, ATTR_NAME, enable)
     return Response(render_btn(context, request))
+
+
+@view_action('proposal_extras', 'start_decisions',
+             interface=IAgendaItem,
+             priority=1)
+def start_decisions_action(context, request, va, **kw):
+    return """<li><a href="{}">{}</a></li>""".format(
+        request.resource_path(context, '_start_decisions'),
+        "Preparera för beslut"
+    )
+
+
+@view_config(context=IAgendaItem, name='_start_decisions', permission=MODERATE_MEETING)
+class StartDecisionsView(BaseView):
+
+    def __call__(self):
+        """ Start the decision process.
+            * Block proposals
+            * Make all proposals that aren't marked with 'bifall yrkat' unhandled.
+        """
+        # context is the agenda item - block proposals
+        self.context.proposal_block = True
+        # Change all proposals that are published to unhandled, if they aren't marked as 'bifall yrkat'
+        count = 0
+        for obj in self.context.values():
+            if IProposal.providedBy(obj) and obj.get_workflow_state() == 'published' and not getattr(obj, ATTR_NAME, False):
+                unrestricted_wf_transition_to(obj, 'unhandled')
+                count += 1
+        self.flash_messages.add("{} förslag justerade".format(count))
+        return HTTPFound(location=self.request.resource_url(self.context))
 
 
 def includeme(config):
